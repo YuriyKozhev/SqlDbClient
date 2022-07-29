95722c6c9f00e285ed9da539a86051d37eb335fe
y.kozhev <y.kozhev@rambler-ca.ru>
2022-07-29 17:55:21 +0300

    create setup.py to support setuptools >= 42.0.0

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
-from setuptools import setup
+import setuptools
 
-setup()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="sql_executor",
+    version="0.0.1",
+    author="Yuriy Kozhev",
+    author_email="yuriy.kozhev@gmail.com",
+    description="A SQL client software package, mainly for use in Jupyter Notebook environment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://example.com",
+    project_urls={
+        "Homepage": "https://example.org",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    license='MIT',
+    license_files = ('LICENSE',),
+    platforms=['any'],
+)
