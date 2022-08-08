0944a4f46fe4a61e015b8a993b3ee6b9833db946
y.kozhev <y.kozhev@rambler-ca.ru>
2022-08-08 14:50:22 +0300

    start writing module-level documentation

--- src/sql_executor/__init__.py
+++ src/sql_executor/__init__.py
@@ -1,5 +1,19 @@
-"""
-Sql Executor
+__docformat__ = "restructuredtext"
+
+# module level doc-string
+__doc__ = """
+sql-executor 
+=====================================================================
+
+**sql-executor ** provides additional functionalities to work with DBMS utilizing 
+mainly 2 powerful packages sqlalchemy and pandas.
+
+
+Main Features
+-------------
+Here are just a few of the things that sql-executor does well:
+
+  - first
 """
 
 from .sql_executor import SqlExecutor
