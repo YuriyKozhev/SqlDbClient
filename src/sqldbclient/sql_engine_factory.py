267e192f92a778c09a4400cbf593843df14be4c5
yuriy.kozhev <yuriy.kozhev@gmail.com>
2022-11-28 02:36:23 +0300

    fix error in SqlEngineFactory for legacy sqlalchemy versions

--- src/sqldbclient/sql_engine_factory.py
+++ src/sqldbclient/sql_engine_factory.py
@@ -7,13 +7,14 @@ from .singleton import Singleton
 
 @class_logifier(['make_engine'])
 class SqlEngineFactory(metaclass=Singleton):
-    def __init__(self, **default_params):
-        self._default_params = default_params
+    def __init__(self, *default_args, **default_kwargs):
+        self._default_args = default_args
+        self._default_kwargs = default_kwargs
 
     def _make_engine(self, **kwargs):
-        engine_params = self._default_params
-        engine_params.update(kwargs)
-        engine = create_engine(**engine_params)
+        engine_kwargs = self._default_kwargs
+        engine_kwargs.update(kwargs)
+        engine = create_engine(*self._default_args, **engine_kwargs)
         engine.execute = method_logifier(engine.execute, Engine)
         return engine
 
