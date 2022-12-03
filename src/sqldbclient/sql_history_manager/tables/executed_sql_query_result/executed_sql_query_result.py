8a90bb4c9265f432365fa563f36616636b352864
yuriy.kozhev <yuriy.kozhev@gmail.com>
2022-12-04 00:49:36 +0300

    fix column estimated_size not found in executed_sql_query_result

--- src/sqldbclient/sql_history_manager/tables/executed_sql_query_result/executed_sql_query_result.py
+++ src/sqldbclient/sql_history_manager/tables/executed_sql_query_result/executed_sql_query_result.py
@@ -3,7 +3,7 @@ from typing import List
 
 import pandas as pd
 from sqlalchemy import Column, Table, ForeignKey
-from sqlalchemy import String
+from sqlalchemy import String, Integer
 
 from ...orm_config import metadata, orm_map, EXECUTED_SQL_QUERY_TABLE_NAME, EXECUTED_SQL_QUERY_RESULT_TABLE_NAME
 from .custom_sqlalchemy_types.data_types import DataTypes
@@ -16,6 +16,7 @@ executed_sql_query_result = Table(
     Column('uuid', String, ForeignKey(f"{EXECUTED_SQL_QUERY_TABLE_NAME}.uuid"),  primary_key=True),
     Column('dataframe', DataFrame),
     Column('datatypes', DataTypes),
+    Column('estimated_size', Integer),
     extend_existing=True,
 )
 
@@ -30,7 +31,7 @@ class ExecutedSqlQueryResult:
     def __post_init__(self):
         self.datatypes = [d.name for d in self.dataframe.dtypes]
         # estimated dataframe size in bytes
-        self.estimated_size = self.dataframe.memory_usage(deep=True).sum()
+        self.estimated_size = int(self.dataframe.memory_usage(deep=True).sum())
 
 
 orm_map(ExecutedSqlQueryResult, executed_sql_query_result)
