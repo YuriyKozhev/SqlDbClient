535459f5660a4478ebd8ae16afcd6fde7c5f1330
yuriy.kozhev <yuriy.kozhev@gmail.com>
2022-12-03 22:42:14 +0300

    fill __init__.py in sql_history_manager

---
+++ src/sqldbclient/sql_history_manager/__init__.py
@@ -0,0 +1,11 @@
+__docformat__ = "restructuredtext"
+
+# module level doc-string
+__doc__ = """
+sql-history-manager
+=====================================================================
+
+**sql-history-manager** keeps tracks of executed sql queries, their results and executions meta info.
+"""
+
+from .sql_history_manager import SqlHistoryManager
