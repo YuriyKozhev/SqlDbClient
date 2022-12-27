348889385738879c65b4511bbaac61dfd7e88109
yuriy.kozhev <yuriy.kozhev@gmail.com>
2022-12-27 23:47:57 +0300

    add _get_connection method to SqlTransactionManager

--- src/sqldbclient/sql_transaction_manager/sql_transaction_manager.py
+++ src/sqldbclient/sql_transaction_manager/sql_transaction_manager.py
@@ -1,5 +1,6 @@
 from typing import Optional
 import time
+import sqlalchemy
 from sqlalchemy.engine.base import Engine, RootTransaction
 from sqldbclient.utils.log_decorators import logger
 from sqldbclient.utils.deprecated import deprecated
@@ -18,10 +19,22 @@ class SqlTransactionManager:
             return False
         return self._transaction.is_active
 
+    def _get_connection(self, outside_transaction: bool = False):
+        if self._is_in_transaction:
+            if outside_transaction:
+                raise ValueError('Unable to get connection outside transaction while in transaction')
+            return self._transaction.connection
+        connection = self._engine.connect()
+        if outside_transaction:
+            # workaround to get outside of implicit transaction
+            connection.execute(sqlalchemy.text('COMMIT'))
+        return connection
+
     def __enter__(self):
         logger.warning(f'Starting transaction')
         self._start = time.perf_counter()
-        self._transaction = self._engine.connect().begin()
+        connection = self._get_connection()
+        self._transaction = connection.begin()
         return self
 
     def __exit__(self, exc_type, exc, exc_tb):
