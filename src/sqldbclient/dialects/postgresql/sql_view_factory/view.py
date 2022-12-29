5d0730d6c3591fbdc44ac88884c361ed0dcd9dc4
yuriy.kozhev <yuriy.kozhev@gmail.com>
2022-12-29 23:39:45 +0300

    create View entity

---
+++ src/sqldbclient/dialects/postgresql/sql_view_factory/view.py
@@ -0,0 +1,24 @@
+import enum
+from typing import Dict, List
+from dataclasses import dataclass, field
+
+
+class ViewType(enum.Enum):
+    REGULAR_VIEW = 'v'
+    MATERIALIZED_VIEW = 'vm'
+
+
+@dataclass
+class View:
+    schema: str
+    name: str
+    view_type: ViewType
+    owner: str
+    definition: str = field(repr=False)
+    privileges: Dict[str, List[str]] = field(repr=False)
+    dependant_objects: List[object] = field(repr=False)
+    dependant_objects_number: List[object] = field(init=False)
+    full_name: str = field(init=False)
+
+    def __post_init__(self):
+        self.full_name = f'"{self.schema}"."{self.name}"'
