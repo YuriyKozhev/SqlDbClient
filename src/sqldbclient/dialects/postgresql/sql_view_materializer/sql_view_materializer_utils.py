388f644d9e15afa7b97215b6ef8914aed4187088
yuriy.kozhev <yuriy.kozhev@gmail.com>
2022-12-30 13:20:16 +0300

    create SqlViewMaterializerUtils to work basic view related operations

---
+++ src/sqldbclient/dialects/postgresql/sql_view_materializer/sql_view_materializer_utils.py
@@ -0,0 +1,74 @@
+from sqldbclient.sql_executor import SqlExecutor
+from sqldbclient.dialects.postgresql.sql_view_factory.view import View, ViewType
+from sqldbclient.utils.log_decorators import logger
+
+
+class SqlViewMaterializerUtils:
+    def __init__(self, view: View, sql_executor: SqlExecutor):
+        self.view = view
+        self.sql_executor = sql_executor
+
+    def set_owner(self) -> None:
+        if self.view.view_type == ViewType.REGULAR_VIEW:
+            self.sql_executor.execute(f"""
+                ALTER VIEW {self.view.full_name} OWNER TO {self.view.owner};
+            """)
+        elif self.view.view_type == ViewType.MATERIALIZED_VIEW:
+            self.sql_executor.execute(f"""
+                ALTER MATERIALIZED VIEW {self.view.full_name} OWNER TO {self.view.owner};
+            """)
+        else:
+            raise Exception('Unexpected error')
+        logger.warning(f'View {self.view.full_name} owner set to {self.view.owner}')
+
+    def set_privileges(self) -> None:
+        for grantee, privileges in self.view.privileges.items():
+            for privilege in privileges:
+                self.sql_executor.execute(f"""
+                    GRANT {privilege} ON {self.view.full_name} TO {grantee};
+                """)
+        logger.warning(f'View {self.view.full_name} privileges set')
+
+    def restore(self) -> None:
+        self.create()
+        self.set_owner()
+        self.set_privileges()
+
+    def drop(self) -> None:
+        if self.view.view_type == ViewType.REGULAR_VIEW:
+            self.sql_executor.execute(f'DROP VIEW {self.view.full_name}')
+        elif self.view.view_type == ViewType.MATERIALIZED_VIEW:
+            self.sql_executor.execute(f'DROP MATERIALIZED VIEW {self.view.full_name}')
+        else:
+            raise Exception('Unexpected error')
+        logger.warning(f'View {self.view.full_name} dropped')
+
+    def create(self) -> None:
+        if self.view.view_type == ViewType.REGULAR_VIEW:
+            query = '\n'.join([f'CREATE VIEW {self.view.full_name} AS', self.view.definition])
+            self.sql_executor.execute(query)
+        elif self.view.view_type == ViewType.MATERIALIZED_VIEW:
+            query = '\n'.join([f'CREATE MATERIALIZED VIEW {self.view.full_name} AS',
+                               self.view.definition.replace(';', ''),
+                               'WITH NO DATA'])
+            self.sql_executor.execute(query)
+        else:
+            raise Exception('Unexpected error')
+        logger.warning(f'Created {self.view.full_name}')
+
+    def copy_privileges_to(self, obj: View):
+        for grantee, privileges in self.view.privileges.items():
+            for privilege in privileges:
+                self.sql_executor.execute(f"""
+                    GRANT {privilege} ON {obj.full_name} TO {grantee};
+                """)
+
+    def refresh(self) -> None:
+        logger.warning(f'Refreshing {self.view.full_name}...')
+        if self.view.view_type == ViewType.REGULAR_VIEW:
+            logger.warning(f'Skipping regular view {self.view.full_name}')
+        elif self.view.view_type == ViewType.MATERIALIZED_VIEW:
+            self.sql_executor.execute(f'REFRESH MATERIALIZED VIEW {self.view.full_name}')
+        else:
+            raise Exception('Unexpected error')
+        logger.warning(f'Refreshed {self.view.full_name}')
