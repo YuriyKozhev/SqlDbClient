0ef13b69a83c7f77d642d045fd7e0d162f6c2cdd
yuriy.kozhev <yuriy.kozhev@gmail.com>
2022-12-30 20:06:26 +0300

    add query_shortened field to ExecutedSqlQuery and set query field repr=False

--- src/sqldbclient/sql_history_manager/tables/executed_sql_query/executed_sql_query.py
+++ src/sqldbclient/sql_history_manager/tables/executed_sql_query/executed_sql_query.py
@@ -6,7 +6,7 @@ import sqlparse
 from sqlalchemy import String, DateTime, Interval
 from sqlalchemy import Table, Column
 
-from ...orm_config import metadata, orm_map, EXECUTED_SQL_QUERY_TABLE_NAME
+from sqldbclient.sql_history_manager.orm_config import metadata, orm_map, EXECUTED_SQL_QUERY_TABLE_NAME
 
 
 executed_sql_query = Table(
@@ -18,6 +18,7 @@ executed_sql_query = Table(
     Column('finish_time', DateTime),
     Column('duration', Interval),
     Column('query_type', String),
+    Column('query_shortened', String),
     extend_existing=True,
 )
 
@@ -25,16 +26,20 @@ executed_sql_query = Table(
 @dataclass
 class ExecutedSqlQuery:
     uuid: str = field(init=False)
-    query: str
+    query: str = field(repr=False)
     start_time: datetime
     finish_time: datetime
     duration: timedelta = field(init=False)
     query_type: str = field(init=False)
+    query_shortened: str = field(init=False)
 
     def __post_init__(self):
         self.duration = self.finish_time.replace(microsecond=self.start_time.microsecond) - self.start_time
         self.uuid = uuid.uuid4().hex
         self.query_type = sqlparse.parse(self.query)[0].get_type()
+        self.query_shortened = self.query
+        if len(self.query_shortened) > 20:
+            self.query_shortened = f'{self.query[:10]} ... {self.query[-10:]}'
 
 
 orm_map(ExecutedSqlQuery, executed_sql_query)
