a154c0b567d012d2c931bcd68e4386d5dd4e6c61
yuriy.kozhev <yuriy.kozhev@gmail.com>
2023-08-06 16:05:36 +0300

    create postgresql utils.py with grant_access function for providing access to database objects

---
+++ src/sqldbclient/dialects/postgresql/utils.py
@@ -0,0 +1,19 @@
+from sqldbclient.sql_executor import SqlExecutor
+
+
+def grant_access(
+        object_name: str,
+        object_schema: str,
+        user_name: str,
+        sql_executor: SqlExecutor,
+        privilege: str = 'SELECT'
+    ):
+    with sql_executor:
+        sql_executor.execute(f'''
+            GRANT USAGE ON SCHEMA "{object_schema}" TO "{user_name}"
+        ''')
+        sql_executor.execute(f'''
+            GRANT {privilege} ON "{object_schema}"."{object_name}" TO "{user_name}"
+        ''')
+        sql_executor.commit()
+
