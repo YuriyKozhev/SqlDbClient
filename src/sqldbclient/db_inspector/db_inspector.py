1dabfe90ec6a1fdd7d2acb88e19e0ae176980cbd
yuriy.kozhev <yuriy.kozhev@gmail.com>
2023-08-07 16:39:23 +0300

    add get_views method to DbInspector, which is consistent across different sqlalchemy versions

--- src/sqldbclient/db_inspector/db_inspector.py
+++ src/sqldbclient/db_inspector/db_inspector.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 import types
 
 import sqlalchemy
@@ -29,10 +29,26 @@ class DBInspector(Inspector):
         """
         print(self.get_columns_repr(table, schema))
 
+    def get_views(self, schema: Optional[str] = None) -> List[str]:
+        """ Wrapping around sqlalchemy Inspector get_view_names function, and since version 2.0.0
+        get_materialized_view_names_function.
+
+        :param schema: schema in database
+        :return: list of view names
+        """
+        views = self.get_view_names()
+        if hasattr(self, 'get_materialized_view_names'):
+            try:
+                views += self.get_materialized_view_names()
+            except NotImplementedError:
+                pass
+        return views
+
 
 def inspect(*args, **kwargs) -> DBInspector:
     """Wrapper around sqlalchemy inspect function, that adds custom methods."""
     inspector = sqlalchemy.inspect(*args, **kwargs)
     setattr(inspector, 'get_columns_repr', types.MethodType(DBInspector.get_columns_repr, inspector))
     setattr(inspector, 'print_columns', types.MethodType(DBInspector.print_columns, inspector))
+    setattr(inspector, 'get_views', types.MethodType(DBInspector.get_views, inspector))
     return inspector
